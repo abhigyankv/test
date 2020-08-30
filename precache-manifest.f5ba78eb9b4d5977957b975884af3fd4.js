db43822ef0283c2160ee911b5308f5ade7d2b597
abhigyankv <abhigyankv.17@nitj.ac.in>
2020-08-30 16:33:11 +0530

    Updates

---
+++ precache-manifest.f5ba78eb9b4d5977957b975884af3fd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57001326b6fb34a1c03fb9a01c820351",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "baace6d2288c9297ae10",
+    "url": "/test/static/css/main.38c83cfb.chunk.css"
+  },
+  {
+    "revision": "657a2a4c81f9476a8d14",
+    "url": "/test/static/js/2.61eaf6e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.61eaf6e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baace6d2288c9297ae10",
+    "url": "/test/static/js/main.378743bd.chunk.js"
+  },
+  {
+    "revision": "6ef814fd58a812a5fda9",
+    "url": "/test/static/js/runtime-main.ca597356.js"
+  }
+]);
